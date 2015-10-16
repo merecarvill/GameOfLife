dd111d3bffff8b534c27e3088596aae44888591f
Spencer <scarvill91@gmail.com>
2015-10-16 11:04:25 -0500

    Initial codretreat version

--- GameOfLife/GameOfLife.swift
+++ GameOfLife/GameOfLife.swift
@@ -1,4 +1,31 @@
+//  Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+//  Any live cell with two or three live neighbours lives on to the next generation.
+//  Any live cell with more than three live neighbours dies, as if by over-population.
+//  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-public func foo() -> Int {
-    return 123
+public class Grid {
+  public init(livingCellLocations: [[Int]]) {
+
+  }
+
+  public func isAliveAt(coordinates: [Int]) -> Bool {
+    return false
+  }
+}
+
+public class Cell {
+  let state: Bool
+
+  public init(alive: Bool) {
+    self.state = alive
+  }
+
+  public var isAlive: Bool {
+    return state
+  }
+
+  public func nextGenerationCell(numberOfLivingNeighbors: Int) -> Cell {
+
+    return Cell(alive: (numberOfLivingNeighbors == 2 && isAlive) || numberOfLivingNeighbors == 3)
+  }
 }
--- GameOfLifeTests/GameOfLifeTests.swift
+++ GameOfLifeTests/GameOfLifeTests.swift
@@ -2,10 +2,87 @@ import Quick
 import Nimble
 import GameOfLife
 
-class GameOfLifeTests: QuickSpec {
-    override func spec() {
-        it("works") {
-            expect(foo()).to(equal(123))
-        }
+class GridTests: QuickSpec {
+  override func spec() {
+    describe("isAliveAt") {
+      it("") {
+        expect(Grid(livingCellLocations: []).isAliveAt([0, 0])).to(beFalse())
+      }
     }
+  }
+}
+
+class CellTests: QuickSpec {
+  override func spec() {
+    describe("Cell") {
+      describe("isAlive") {
+        it("returns true when it is alive") {
+          expect(Cell(alive: true).isAlive).to(beTrue())
+        }
+
+        it("returns false when it is not alive") {
+          expect(Cell(alive: false).isAlive).to(beFalse())
+        }
+      }
+
+      describe("nextGenerationCell") {
+        it("returns a new cell") {
+          let cell = Cell(alive: true)
+          let numberOfLivingNeighbors = 0
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors)).notTo(beIdenticalTo(cell))
+        }
+
+        it("returns a cell that is not alive if it has less than 2 living neighbors") {
+          let cell = Cell(alive: true)
+          let numberOfLivingNeighbors = 1
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beFalse())
+        }
+
+        it("returns a cell that is alive if an alive cell has 2 alive neighbors"){
+          let cell = Cell(alive: true)
+          let numberOfLivingNeighbors = 2
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beTrue())
+        }
+
+        it("returns a cell that is alive if an alive cell has 3 alive neighbors"){
+          let cell = Cell(alive: true)
+          let numberOfLivingNeighbors = 3
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beTrue())
+        }
+
+        it("returns a cell that is not alive if it has more than 3 living neighbors") {
+          let cell = Cell(alive: true)
+          let numberOfLivingNeighbors = 4
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beFalse())
+        }
+
+        it("returns a cell that is alive if a dead cell has 3 alive neighbors"){
+          let cell = Cell(alive: false)
+          let numberOfLivingNeighbors = 3
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beTrue())
+        }
+
+        it("returns a cell that is dead if a dead cell has less than 3 alive neighbors"){
+          let cell = Cell(alive: false)
+          let numberOfLivingNeighbors = 2
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beFalse())
+        }
+
+        it("returns a cell that is dead if a dead cell has more than 3 alive neighbors"){
+          let cell = Cell(alive: false)
+          let numberOfLivingNeighbors = 4
+
+          expect(cell.nextGenerationCell(numberOfLivingNeighbors).isAlive).to(beFalse())
+        }
+
+      }
+    }
+  }
 }
